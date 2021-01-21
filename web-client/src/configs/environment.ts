07e9af01019e48708bb20c6bfcd13991f6ece453
Nicolas Jardim <nicolasemkis@gmail.com>
2021-01-21 02:06:45 -0300

    feat: add api url to env mapper

--- web-client/src/configs/environment.ts
+++ web-client/src/configs/environment.ts
@@ -1,4 +1,5 @@
 export const environment = Object.freeze({
   appTitle: process.env.VUE_APP_TITLE || '[dev] Clippy Cloud',
   appUrl: process.env.VUE_APP_BASE_URL || '/',
+  apiUrl: process.env.VUE_APP_API_URL || 'http://localhost:3333',
 })

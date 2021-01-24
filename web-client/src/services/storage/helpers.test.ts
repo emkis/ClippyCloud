bf021fc4ffb9e4d6a70aa25659741d5f0b97e43a
emkis <nicolasemkis@gmail.com>
2021-01-24 18:14:13 -0300

    test: add storage helpers tests

---
+++ web-client/src/services/storage/helpers.test.ts
@@ -0,0 +1,61 @@
+import { STORAGE_PREFIX } from './index'
+import { isValidJSON, makeCustomKey, validateStorageItem } from './helpers'
+import { LocalStorageMock } from './__mocks__/localStorage'
+
+describe('isValidJSON()', () => {
+  const invalidJSONList = ['', 'lorem ipsum', '{', `{"name": "emkis",}`]
+
+  invalidJSONList.forEach((json) => {
+    test(`"${json}" should return "false"`, () => {
+      expect(isValidJSON(json)).toBe(false)
+    })
+  })
+
+  const validJSONList = [
+    `{ "name": "emkis" }`,
+    `{ "age": 22 }`,
+    `{ "name": "emkis", "email": null }`,
+  ]
+
+  validJSONList.forEach((json) => {
+    test(`"${json}" should return "true"`, () => {
+      expect(isValidJSON(json)).toBe(true)
+    })
+  })
+})
+
+describe('makeCustomKey()', () => {
+  it('should return only the storage prefix', () => {
+    const key = makeCustomKey('')
+    expect(key).toBe(STORAGE_PREFIX)
+  })
+
+  it('should return a the correct customKey', () => {
+    const customkey1 = makeCustomKey('user')
+    const customkey2 = makeCustomKey('LoremIpsum')
+
+    expect(customkey1).toBe(`${STORAGE_PREFIX}user`)
+    expect(customkey2).toBe(`${STORAGE_PREFIX}LoremIpsum`)
+  })
+})
+
+describe('validateStorageItem()', () => {
+  it('should return "false" if item in storage is not JSON parsable', () => {
+    const itemResponse = validateStorageItem('auth')
+    expect(itemResponse).toBe(false)
+  })
+
+  it('should return remove the storage item if is invalid or not exists', () => {
+    const itemResponse = validateStorageItem('user')
+    expect(itemResponse).toBe(false)
+  })
+
+  it('should return "true" if item in storage is JSON parsable', () => {
+    LocalStorageMock.setItem('username', '{ "name": "emkis" }')
+
+    const itemResponse = validateStorageItem('username')
+    expect(itemResponse).toBe(true)
+  })
+})
+
+beforeEach(jest.clearAllMocks)

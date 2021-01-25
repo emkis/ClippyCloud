30219f97f8e56d4d69e2ae21838aa7f070bb4313
emkis <nicolasemkis@gmail.com>
2021-01-24 21:11:22 -0300

    feat: adds user hook

---
+++ web-client/src/hooks/user/index.ts
@@ -0,0 +1,29 @@
+import { reactive, readonly } from 'vue'
+import { v4 as uuid } from 'uuid'
+
+import { IUserHook, IUserState } from './types'
+import { getFromStorage, saveInStorage } from '@/services/storage'
+
+const state = reactive({
+  id: '',
+}) as IUserState
+
+export function useUser(): IUserHook {
+  return { user: readonly(state) }
+}
+
+function generateId(): string {
+  return uuid()
+}
+
+export function setUserId(targetId: string): void {
+  state.id = targetId
+  saveInStorage('user', state)
+}
+
+export async function initializeUser(): Promise<void> {
+  const user: IUserState = getFromStorage('user')
+
+  if (user) setUserId(user.id)
+  else setUserId(generateId())
+}
---
+++ web-client/src/hooks/user/types.ts
@@ -0,0 +1,7 @@
+export interface IUserHook {
+  user: Readonly<IUserState>
+}
+
+export interface IUserState {
+  id: string
+}

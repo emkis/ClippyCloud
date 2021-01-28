3eadc016221d040a4c334e9ac4cfe94047bd8a51
emkis <nicolasemkis@gmail.com>
2021-01-28 00:44:49 -0300

    refactor(api): changes mongo default url

--- api/src/config/env.js
+++ api/src/config/env.js
@@ -3,7 +3,7 @@ import 'dotenv/config'
 function makeEnv() {
   const appPort = process.env.PORT || 3333
   const appUrl = process.env.APP_URL || `http://localhost:${appPort}`
-  const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/dropbox'
+  const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/clippy-cloud'
 
   return {
     appPort,

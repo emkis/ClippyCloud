7db11fb75c8ef802c5d8816f7a839976d264f971
emkis <nicolasemkis@gmail.com>
2021-01-28 23:35:35 -0300

    refactor(api): changes routes folders to users

--- api/src/routes.js
+++ api/src/routes.js
@@ -3,20 +3,21 @@ import multer from 'multer'
 import multerConfig from './config/multer'
 
 import { FileController } from './controllers/FileController'
-import { FolderController } from './controllers/FolderController'
+import { UserController } from './controllers/UserController'
 import { WakeUpController } from './controllers/WakeUpController'
 
 const routes = express.Router()
 
 routes.get('/wake-up', WakeUpController.index)
-routes.get('/folder/:id', FolderController.show)
-routes.get('/folders', FolderController.index)
+
+routes.get('/user/:id', UserController.show)
+routes.get('/users', UserController.index)
+routes.post('/user', UserController.store)
+
 routes.get('/file/:id', FileController.show)
-routes.post('/folder', FolderController.store)
-routes.delete('/folder/:id', FolderController.delete)
 
 routes.post(
-  '/folder/:id/files',
+  '/user/:id/files',
   multer(multerConfig).single('file'), FileController.store,
 )
 

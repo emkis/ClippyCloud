17982a66d3a80b6488df8913870e61a7aeee9b8c
emkis <nicolasemkis@gmail.com>
2021-01-29 02:00:27 -0300

    chore(api deps): bumps mongoose

--- web-client/src/App.vue
+++ web-client/src/App.vue
@@ -4,8 +4,17 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { useUser, initializeUser } from '@/hooks/user'
+import { WakeUpApi } from '@/services/api/wake-up-api'
 
 export default defineComponent({
   name: 'App',
+  setup() {
+    initializeUser()
+    WakeUpApi.init()
+    const { user } = useUser()
+
+    return { user }
+  },
 })
 </script>
--- web-client/src/pages/Home.vue
+++ web-client/src/pages/Home.vue
@@ -1,14 +1,64 @@
 <template>
   <div class="Home">
-    <h1>Clippy Cloud</h1>
+    <h1 @dragover="handleDrag" @dragleave.prevent="handleDrop">Clippy Cloud</h1>
+
+    <div v-bind="getRootProps({ className: 'aaa' })">
+      <input v-bind="getInputProps()" />
+      <p v-if="isDragActive">Drop the files here ...</p>
+      <p v-else>Drag 'n' drop some files here, or click to select files</p>
+    </div>
+
+    <button @click="open">open</button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { useDropzone } from 'vue3-dropzone'
 
 export default defineComponent({
   name: 'Home',
   components: {},
+  setup() {
+    // https://react-dropzone.js.org/
+    function onDrop(acceptFiles: any, rejectReasons: any) {
+      console.log({ acceptFiles })
+      console.log({ rejectReasons })
+
+      const [file] = acceptFiles
+
+      const url = URL.createObjectURL(file)
+      console.log(url)
+    }
+
+    const {
+      getRootProps,
+      getInputProps,
+      isDragActive,
+      open,
+      draggedFiles,
+      acceptedFiles,
+    } = useDropzone({
+      onDrop,
+      accept: ['image/jpeg'],
+    })
+
+    const handleDrag = () => {
+      console.log('handleDrag')
+    }
+
+    const handleDrop = () => {
+      console.log('handleDrop')
+    }
+
+    return {
+      handleDrag,
+      handleDrop,
+      getRootProps,
+      getInputProps,
+      isDragActive,
+      open,
+    }
+  },
 })
 </script>

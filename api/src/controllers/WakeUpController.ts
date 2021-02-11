6fad1ff5555b5a0cea10504ac8c8b6c376bc77f6
emkis <nicolasemkis@gmail.com>
2021-02-10 23:35:51 -0300

    refactor(api): changes arg name

--- api/src/controllers/WakeUpController.ts
+++ api/src/controllers/WakeUpController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express'
 
 export const WakeUpController = {
-  index (_: Request, response: Response) {
+  index (_request: Request, response: Response) {
     return response.status(204).send()
   },
 }

bd083c3cbd24479951f936947ee86b1a8bde005f
emkis <nicolasemkis@gmail.com>
2021-02-18 03:05:29 -0300

    test(client): add button tests

---
+++ web-client/src/components/Button/Button.test.ts
@@ -0,0 +1,35 @@
+import { render } from '@testing-library/vue'
+import Button from '@/components/Button/index.vue'
+
+jest.mock('./types', () => ({
+  Themes: { Default: 'default' },
+}))
+
+describe('<Button />', () => {
+  it('emit an event when clicked', async () => {
+    const wrapper = render(Button, {
+      slots: { default: 'Cool Button' },
+    })
+
+    const button = await wrapper.findByText('Cool Button')
+    button.click()
+
+    const emittedEvents = wrapper.emitted()
+
+    expect(emittedEvents).toHaveProperty('onClick')
+  })
+
+  it(`should not emit an event when is clicked`, async () => {
+    const wrapper = render(Button, {
+      props: { disabled: true },
+      slots: { default: 'Cool Button' },
+    })
+
+    const button = await wrapper.findByText('Cool Button')
+    button.click()
+
+    const emittedEvents = wrapper.emitted()
+
+    expect(emittedEvents).toMatchObject({})
+  })
+})

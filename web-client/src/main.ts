3e9d93363b2bff157db68a748d4f46750943e170
emkis <nicolasemkis@gmail.com>
2021-02-17 22:19:00 -0300

    style: refactor global styles

--- web-client/src/main.ts
+++ web-client/src/main.ts
@@ -3,7 +3,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import '@/styles/global.scss'
+import '@/styles/global/index.scss'
 
 const app = createApp(App)
 

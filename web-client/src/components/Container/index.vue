ca9026d6ee774d89738927cb370d15ad5eb5873c
emkis <nicolasemkis@gmail.com>
2021-02-18 03:52:42 -0300

    feat(client): adds container component

---
+++ web-client/src/components/Container/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="Container">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Container',
+})
+</script>
+
+<style lang="scss" scoped>
+.Container {
+  padding: rem(120px 15px);
+
+  @media (min-width: 43.75em) {
+    padding: rem(160px 20px);
+  }
+}
+</style>

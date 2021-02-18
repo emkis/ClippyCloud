72694ba8f7a01a3928c273ac7abb0a27c237ab1d
emkis <nicolasemkis@gmail.com>
2021-02-18 03:32:23 -0300

    feat(client): adds text component

---
+++ web-client/src/components/Text/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <component :is="as" class="Text">
+    <slot />
+  </component>
+</template>
+
+<script lang="ts">
+import type { Text } from './types'
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+  props: {
+    as: { type: String as PropType<Text>, required: true },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.Text {
+  font-size: rem(18px);
+  font-weight: 400;
+  color: var(--color-cadet-blue);
+
+  @media (min-width: 43.75em) {
+    font-size: rem(20px);
+  }
+}
+</style>
---
+++ web-client/src/components/Text/types.ts
@@ -0,0 +1,1 @@
+export type Text = 'p' | 'span' | 'strong'

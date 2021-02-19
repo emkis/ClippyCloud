b8cebe349414aa4e8d402c48fe3b7631eaf0bbea
emkis <nicolasemkis@gmail.com>
2021-02-19 02:15:06 -0300

    feat(client): adds icon default structure

---
+++ web-client/src/components/Icons/defaultProps.ts
@@ -0,0 +1,10 @@
+export const iconDefaultProps = {
+  color: {
+    type: String,
+    default: 'currentColor',
+  },
+  size: {
+    type: [String, Number],
+    default: '24',
+  },
+}
---
+++ web-client/src/components/Icons/index.ts
@@ -0,0 +1,1 @@
+export { default as IconTimer } from './IconTimer.vue'

2550626db047af963532e13267b7674246a710e4
emkis <nicolasemkis@gmail.com>
2021-02-19 03:46:58 -0300

    feat(client): adds all basic pages

---
+++ web-client/src/pages/MyUploads/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>My Uploads</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useRoute } from 'vue-router'
+
+export default defineComponent({
+  setup() {
+    const { meta } = useRoute()
+
+    document.title = `${document.title} | ${meta.title}`
+
+    return {}
+  },
+})
+</script>
+
+<style scoped></style>
--- web-client/src/router/index.ts
+++ web-client/src/router/index.ts
@@ -1,13 +1,37 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-
 import { environment } from '@/configs/environment'
+
 import Home from '../pages/Home/index.vue'
+const Upload = import(
+  /* webpackChunkName: "Upload" */
+  '../pages/Upload/index.vue'
+)
+const MyUploads = import(
+  /* webpackChunkName: "MyUploads" */
+  '../pages/MyUploads/index.vue'
+)
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/upload',
+    name: 'Upload',
+    component: Upload,
+    meta: {
+      title: 'Upload',
+    },
+  },
+  {
+    path: '/my-uploads',
+    name: 'MyUploads',
+    component: MyUploads,
+    meta: {
+      title: 'My Uploads',
+    },
   },
 ]
 

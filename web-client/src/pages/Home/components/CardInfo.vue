868e032c5ed8aacbf04eb40dafac06887b4cf3d0
emkis <nicolasemkis@gmail.com>
2021-02-21 21:16:06 -0300

    refactor(client): inject colors from theme service

--- web-client/src/pages/Home/components/CardInfo.vue
+++ web-client/src/pages/Home/components/CardInfo.vue
@@ -14,6 +14,7 @@
 
 <script lang="ts">
 import { computed, defineComponent } from 'vue'
+import { EThemeConcepts } from '@/services/theme'
 
 import Heading from '@/components/Heading/index.vue'
 import Text from '@/components/Text/index.vue'
@@ -23,7 +24,7 @@ export default defineComponent({
   components: { Heading, Text },
   props: {
     title: { type: String, required: true },
-    color: { type: String, default: 'var(--color-bright-gray)' },
+    color: { type: String, default: EThemeConcepts.overBackgroundElement },
   },
   setup(props) {
     const computedBackground = computed(() => ({ background: props.color }))
@@ -39,7 +40,7 @@ export default defineComponent({
   place-content: center;
   text-align: center;
   border-radius: $border-radius-m;
-  background: var(--color-charade);
+  background: var(--concept-over-background);
 
   &__circle {
     $circleSize: 150px;
--- web-client/src/pages/Home/sections/SectionHowItWorks.vue
+++ web-client/src/pages/Home/sections/SectionHowItWorks.vue
@@ -8,27 +8,36 @@
       </Text>
 
       <div class="SectionHowItWorks__cards-container">
-        <CardInfo title="All Files Expire" color="var(--color-kournikova-25)">
+        <CardInfo
+          title="All Files Expire"
+          :color="makeRgba(EThemeColors.kournikovaRGB, cardOpacity)"
+        >
           <template #icon>
-            <IconTimer color="var(--color-kournikova)" size="54" />
+            <IconTimer :color="EThemeColors.kournikova" size="54" />
           </template>
 
           After 30 minutes, all files expire automatically. Then you can no
           longer access them.
         </CardInfo>
 
-        <CardInfo title="No Login Required" color="var(--color-geraldine-25)">
+        <CardInfo
+          title="No Login Required"
+          :color="makeRgba(EThemeColors.geraldineRGB, cardOpacity)"
+        >
           <template #icon>
-            <IconFast color="var(--color-geraldine)" size="54" />
+            <IconFast :color="EThemeColors.geraldine" size="54" />
           </template>
 
           No data from you will be saved. As long as you stay in the same
           browser, you can see all your uploaded files.
         </CardInfo>
 
-        <CardInfo title="For Free Forever" color="var(--color-heliotrope-25)">
+        <CardInfo
+          title="For Free Forever"
+          :color="makeRgba(EThemeColors.heliotropeRGB, cardOpacity)"
+        >
           <template #icon>
-            <IconFlower color="var(--color-heliotrope)" size="54" />
+            <IconFlower :color="EThemeColors.heliotrope" size="54" />
           </template>
 
           You will never need to pay for this service. It was created with the
@@ -41,6 +50,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { EThemeColors, makeRgba } from '@/services/theme'
 
 import Container from '@/components/Container/index.vue'
 import Heading from '@/components/Heading/index.vue'
@@ -58,6 +68,11 @@ export default defineComponent({
     IconTimer,
     IconFlower,
     IconFast,
+  },
+  setup() {
+    const cardOpacity = 0.25
+
+    return { EThemeColors, makeRgba, cardOpacity }
   },
 })
 </script>

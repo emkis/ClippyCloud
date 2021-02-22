e4c79137c7ae0b4180262139597e55b09902a2e1
emkis <nicolasemkis@gmail.com>
2021-02-21 21:29:19 -0300

    feat(client): add new icon

---
+++ web-client/src/components/Icons/IconArrowLeft.vue
@@ -0,0 +1,35 @@
+<template>
+  <svg
+    viewBox="0 0 40 32"
+    fill="none"
+    :stroke="color"
+    :height="size"
+    :width="size"
+    xmlns="http://www.w3.org/2000/svg"
+    role="presentation"
+  >
+    <title>Icon representing go back to previous step</title>
+    <path
+      d="M1.6 16L38.4 16"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M4.80078 21.001L0.799976 16L4.80078 10.999"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { iconDefaultProps } from './defaultProps'
+
+export default defineComponent({
+  name: 'IconArrowLeft',
+  props: { ...iconDefaultProps },
+})
+</script>

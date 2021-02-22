13c185f6154a26dd495e801299ba4fcdf9ebcfc6
emkis <nicolasemkis@gmail.com>
2021-02-21 21:19:24 -0300

    chore(client eslint): disable explicit return type rule

--- web-client/.eslintrc.js
+++ web-client/.eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
   rules: {
     semi: ['error', 'never'],
     'prettier/prettier': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },

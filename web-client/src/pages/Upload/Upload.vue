81197955cc2dea506848d1c68980751406739003
emkis <nicolasemkis@gmail.com>
2021-02-23 02:12:47 -0300

    feat(client): add page descriptions and styles

--- web-client/src/pages/Upload/Upload.vue
+++ web-client/src/pages/Upload/Upload.vue
@@ -1,8 +1,16 @@
 <template>
   <Navbar linkName="My Uploads" routeName="MyUploads" />
 
-  <Container>
-    <h1>Upload</h1>
+  <Container class="UploadContainer">
+    <Heading class="UploadContainer__title" level="1">
+      Upload your files
+    </Heading>
+    <Text>The files should be maximum of 100mb.</Text>
+
+    <FileUploader
+      class="UploadContainer__file-uploader"
+      @onDrop="handleDropFiles"
+    />
   </Container>
 </template>
 
@@ -13,18 +21,43 @@ import { useRoute } from 'vue-router'
 import { usePageTitle } from '@/hooks/page-title'
 
 import { Navbar } from '@/components/Navbar'
+import { Heading } from '@/components/Heading'
+import { Text } from '@/components/Text'
 import { Container } from '@/components/Container'
+import { FileUploader } from './components/FileUploader'
 
 export default defineComponent({
   name: 'Upload',
-  components: { Navbar, Container },
+  components: { Navbar, Container, Heading, Text, FileUploader },
   setup() {
     const { meta } = useRoute()
     const { setTitle } = usePageTitle()
 
     setTitle(meta.title as string)
+
+    const handleDropFiles = (files: File[]) => console.log({ files })
+
+    return { handleDropFiles }
   },
 })
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.UploadContainer {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  &__title {
+    margin-bottom: rem(24px);
+  }
+
+  &__file-uploader {
+    width: 100%;
+    max-width: rem(600px);
+    margin-top: rem(72px);
+  }
+}
+</style>

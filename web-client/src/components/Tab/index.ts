1df167da88789c93645ba4b5da090d6f7557777e
emkis <nicolasemkis@gmail.com>
2021-02-24 23:16:51 -0300

    feat(client): add files for tab components

--- web-client/src/components/Tab/index.ts
+++ web-client/src/components/Tab/index.ts
@@ -1,2 +1,4 @@
+export { useTab } from './hook'
 export { default as TabContainer } from './TabContainer.vue'
 export { default as TabItem } from './TabItem.vue'
+export { default as TabLayout } from './TabLayout.vue'
---
+++ web-client/src/components/Tab/types.ts
@@ -0,0 +1,10 @@
+export interface ITabItemProps {
+  label: string
+  items: number
+}
+
+export const enum ETabComponents {
+  Tab = 'Tab',
+  TabItem = 'TabItem',
+  TabLayout = 'TabLayout',
+}

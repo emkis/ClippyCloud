6e74d7141408ee43cd797e4a358954575f6c00a1
emkis <nicolasemkis@gmail.com>
2021-02-24 23:55:08 -0300

    feat(client): add tab container

--- web-client/src/components/Tab/TabContainer.vue
+++ web-client/src/components/Tab/TabContainer.vue
@@ -1,27 +1,72 @@
 <template>
   <div class="TabContainer">
-    <slot />
+    <nav class="TabContainer__tabs">
+      <TabItem
+        :name="tab.props.name"
+        :key="tabIndex"
+        v-for="(tab, tabIndex) in tabs"
+        @onSelected="handleTabChange"
+      />
+    </nav>
+
+    <div class="TabContainer__content">
+      <slot />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { ITabItemProps, ETabComponents } from './types'
+
+import { defineComponent, onBeforeMount, ref } from 'vue'
+import { useTab } from './hook'
+
+import TabItem from './TabItem.vue'
 
 export default defineComponent({
   name: 'TabContainer',
-  props: {},
-  setup() {
-    return {}
+  components: { TabItem },
+  setup(props, { slots }) {
+    const tabs = ref<ITabItemProps[]>([])
+    const { setActiveTab } = useTab()
+
+    onBeforeMount(getTabChildren)
+
+    function handleTabChange(targetTabName: string) {
+      setActiveTab(targetTabName)
+    }
+
+    function getTabChildren() {
+      if (!slots.default) return
+
+      const childrenProps = slots
+        .default()
+        .filter(
+          (child: any) => child.type!.name! === ETabComponents.TabLayout
+        ) as any[]
+
+      tabs.value = childrenProps
+    }
+
+    return { tabs, handleTabChange }
   },
 })
 </script>
 
 <style lang="scss" scoped>
 .TabContainer {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: $border-radius-s;
-  overflow: auto;
+  width: 100%;
+
+  &__tabs {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: $border-radius-s;
+    overflow: auto;
+  }
+
+  &__content {
+    margin-top: rem(24px);
+  }
 }
 </style>

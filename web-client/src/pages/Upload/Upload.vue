6f06abc535bb4e2cf5d7f100d83a7d0e63819336
emkis <nicolasemkis@gmail.com>
2021-02-24 23:57:24 -0300

    feat(client): add tabs in page

--- web-client/src/pages/Upload/Upload.vue
+++ web-client/src/pages/Upload/Upload.vue
@@ -1,16 +1,36 @@
 <template>
   <Navbar linkName="My Uploads" routeName="MyUploads" />
 
-  <Container class="UploadContainer">
-    <Heading class="UploadContainer__title" level="1">
+  <Container class="UploaderContainer">
+    <Heading class="UploaderContainer__title" level="1">
       Upload your files
     </Heading>
     <Text>The files should be maximum of 100mb.</Text>
 
     <FileUploader
-      class="UploadContainer__file-uploader"
+      class="UploaderContainer__file-uploader"
       @onDrop="handleDropFiles"
     />
+  </Container>
+
+  <Container class="FilesContainer">
+    <Heading class="FilesContainer__title" level="3">
+      Choose file status
+    </Heading>
+
+    <TabContainer>
+      <TabLayout name="Available">
+        <h4>Im a tab content in tab 1</h4>
+      </TabLayout>
+
+      <TabLayout name="Expired">
+        <h4>Im a tab content in tab 2</h4>
+      </TabLayout>
+
+      <TabLayout name="Availables">
+        <h4>Im a tab content in tab 3</h4>
+      </TabLayout>
+    </TabContainer>
   </Container>
 </template>
 
@@ -25,15 +45,26 @@ import { Heading } from '@/components/Heading'
 import { Text } from '@/components/Text'
 import { Container } from '@/components/Container'
 import { FileUploader } from './components/FileUploader'
+import { TabContainer, TabLayout, useTab } from '@/components/Tab'
 
 export default defineComponent({
   name: 'Upload',
-  components: { Navbar, Container, Heading, Text, FileUploader },
+  components: {
+    Navbar,
+    Container,
+    Heading,
+    Text,
+    FileUploader,
+    TabContainer,
+    TabLayout,
+  },
   setup() {
     const { meta } = useRoute()
     const { setTitle } = usePageTitle()
+    const { setActiveTab } = useTab()
 
     setTitle(meta.title as string)
+    setActiveTab('Available')
 
     const handleDropFiles = (files: File[]) => console.log({ files })
 
@@ -43,7 +74,7 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.UploadContainer {
+.UploaderContainer {
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -58,6 +89,14 @@ export default defineComponent({
     width: 100%;
     max-width: rem(600px);
     margin-top: rem(72px);
+  }
+}
+
+.FilesContainer {
+  padding-top: 0;
+
+  &__title {
+    margin-bottom: rem(24px);
   }
 }
 </style>

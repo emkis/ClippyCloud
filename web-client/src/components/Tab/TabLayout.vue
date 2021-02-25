784fd1d62b0b72c8d8b0cd6931ff211e0b6aff0e
emkis <nicolasemkis@gmail.com>
2021-02-24 23:16:00 -0300

    feat(client): add tab layout component

---
+++ web-client/src/components/Tab/TabLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="TabLayout">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useTab } from './hook'
+
+export default defineComponent({
+  name: 'TabLayout',
+  props: {
+    name: { type: String, required: true },
+  },
+  setup() {
+    useTab()
+    return {}
+  },
+})
+</script>

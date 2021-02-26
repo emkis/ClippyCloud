5369a078da5fa62ff09c5dd58ea91b4b0bb8cdfd
emkis <nicolasemkis@gmail.com>
2021-02-26 02:01:09 -0300

    feat(client): execute main hooks after app mount

--- web-client/src/main.ts
+++ web-client/src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
-import { initializeUser } from '@/hooks/user'
 
-import App from './App.vue'
-import router from './router'
+import { useUser } from '@/hooks/user'
+import { usePageTitle } from '@/hooks/page-title'
+import { environment } from '@/configs/environment'
+import { router } from '@/router'
 
+import App from '@/App.vue'
 import '@/styles/global/index.scss'
 
 const app = createApp(App)
@@ -11,4 +13,8 @@ const app = createApp(App)
 app.use(router)
 app.mount('#app')
 
+const { initializeUser } = useUser()
+const { setTitle } = usePageTitle({ appendTitle: false })
+
 initializeUser()
+setTitle(environment.appTitle)

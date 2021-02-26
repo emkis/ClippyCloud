028afc7916e0eca4e7de281dfdc4bcbb0548ff51
emkis <nicolasemkis@gmail.com>
2021-02-26 01:36:32 -0300

    refactor(client): change import path

--- web-client/src/router/lazyRoutes.ts
+++ web-client/src/router/lazyRoutes.ts
@@ -1,11 +1,11 @@
 import { Component } from 'vue'
 
 export const Upload = (): Promise<Component> => {
-  return import(/* webpackChunkName: "Upload" */ '../pages/Upload/Upload.vue')
+  return import(/* webpackChunkName: "Upload" */ '@/pages/Upload/Upload.vue')
 }
 
 export const MyUploads = (): Promise<Component> => {
   return import(
-    /* webpackChunkName: "MyUploads" */ '../pages/MyUploads/MyUploads.vue'
+    /* webpackChunkName: "MyUploads" */ '@/pages/MyUploads/MyUploads.vue'
   )
 }

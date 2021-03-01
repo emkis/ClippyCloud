a70f25993d5f815c6036e32bf2c51e260425be5f
emkis <nicolasemkis@gmail.com>
2021-02-28 22:19:38 -0300

    feat(client): add filecard and helpers

--- web-client/src/components/Cards/FileCard/FileCard.vue
+++ web-client/src/components/Cards/FileCard/FileCard.vue
@@ -1,21 +1,16 @@
 <template>
-  <BaseCard
-    class="FileCard"
-    :fileName="fileName"
-    :rightMessage="expiredStatus"
-    :leftMessage="formattedFileSize"
-  >
+  <BaseCard class="FileCard" :fileName="fileName">
     <template #top>
-      <div class="FileCard__head-container" :style="fileColor.background">
-        <IconFile class="FileCard__icon" :color="fileColor.icon" size="124" />
-        <h5 class="FileCard__extension">{{ fileExtension || 'PNG' }}</h5>
-      </div>
+      <FileCardHead :fileExtension="fileExtension" />
     </template>
 
-    <template #bottom v-if="!isFileExpired">
-      <Button :theme="actionTheme" @onClick="handleAction">
-        {{ actionText }}
-      </Button>
+    <template #details>
+      <span>{{ formattedFileSize }}</span>
+      <span :style="fileStatusStyle">{{ fileTimeStatus }}</span>
+    </template>
+
+    <template #bottom v-if="!isFileAlreadyExpired">
+      <Button theme="primary" @onClick="handleAction">Copy Link</Button>
     </template>
   </BaseCard>
 </template>
@@ -24,50 +19,52 @@
 import filesize from 'filesize'
 import { computed, defineComponent } from 'vue'
 
-import { defaultCardProps } from './defaultProps'
-import { EThemeColors, makeRgba } from '@/services/theme'
+import { EThemeColors } from '@/services/theme'
+import { defaultBaseCardProps } from '../defaultBaseCardProps'
+import { defaultFileCardProps } from './defaultFileCardProps'
+import { getTimeToExpire, isFileExpired } from './timeHelpers'
 
-import { Button } from '../Button'
-import BaseCard from './BaseCard.vue'
-import IconFile from '@/components/Icons/IconFile.vue'
+import { Button } from '@/components/Button'
+import BaseCard from '../BaseCard.vue'
+import FileCardHead from './FileCardHead.vue'
+
+function getReadableSize(size: number) {
+  const sizeFormatter = filesize.partial({ round: 0 })
+  return sizeFormatter(size)
+}
 
 export default defineComponent({
   name: 'FileCard',
   props: {
-    ...defaultCardProps,
-    fileUrl: { type: String, default: '' },
+    ...defaultBaseCardProps,
+    ...defaultFileCardProps,
     fileSize: { type: Number, required: true },
-    fileExtension: { type: String, required: true },
-    timeToExpire: { type: Number, required: true },
+    createdAt: { type: String, required: true },
   },
   emits: ['onActionClick'],
-  components: { BaseCard, Button, IconFile },
+  components: { BaseCard, FileCardHead, Button },
   setup(props, { emit }) {
-    const isFileExpired = computed(() => false)
-    const expiredStatus = computed(() =>
-      isFileExpired.value ? 'Expired' : `2min to expire`
-    )
+    const isFileAlreadyExpired = computed(() => isFileExpired(props.createdAt))
 
-    const formattedFileSize = computed(() => filesize(props.fileSize || 449248))
-    const fileColor = {
-      icon: EThemeColors.grannySmithApple,
-      background: {
-        'background-color': makeRgba(EThemeColors.grannySmithAppleRGB, 0.25),
-      },
-    }
+    const fileTimeStatus = computed(() => {
+      const minutes = getTimeToExpire(props.createdAt)
+      return isFileAlreadyExpired.value ? 'Expired' : `${minutes} to expire`
+    })
 
-    const actionText = computed(() => 'Copy Link')
-    const actionTheme = computed(() => 'default')
+    const formattedFileSize = computed(() => getReadableSize(props.fileSize))
+
+    const fileStatusStyle = computed(() => {
+      return isFileAlreadyExpired.value && `color: ${EThemeColors.kournikova}`
+    })
+
     const handleAction = () => emit('onActionClick')
 
     return {
-      isFileExpired,
-      expiredStatus,
+      isFileAlreadyExpired,
+      fileTimeStatus,
       formattedFileSize,
-      actionText,
-      actionTheme,
       handleAction,
-      fileColor,
+      fileStatusStyle,
     }
   },
 })
@@ -76,25 +73,6 @@ export default defineComponent({
 <style lang="scss" scoped>
 .FileCard {
   user-select: none;
-
-  &__head-container {
-    position: relative;
-    display: grid;
-    place-content: center;
-    padding: rem(25px);
-    background: none;
-  }
-
-  &__extension {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    font-size: rem(24px);
-  }
-
-  &__icon {
-    opacity: 0.3;
-  }
+  flex: 1;
 }
 </style>

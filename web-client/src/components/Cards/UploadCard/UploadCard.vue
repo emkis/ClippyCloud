0b49382635601765b17a18e2c4b71263a690b78e
emkis <nicolasemkis@gmail.com>
2021-03-01 23:27:22 -0300

    feat(client): add upload card structure

--- web-client/src/components/Cards/UploadCard/UploadCard.vue
+++ web-client/src/components/Cards/UploadCard/UploadCard.vue
@@ -1,16 +1,23 @@
 <template>
-  <BaseCard>
+  <BaseCard class="UploadCard" fileName="HelloWorld.ts">
     <template #top>
-      <div></div>
+      <div class="UploadCard__head">
+        <div class="UploadCard__circle">
+          <h4>74%</h4>
+        </div>
+      </div>
     </template>
 
     <template #details>
-      <span v-if="hasMessage" :style="statusStyle">{{ statusMessage }}</span>
+      <span v-if="hasStatusMessage" :style="statusStyle">
+        {{ statusMessage }}
+      </span>
+
       <span v-else>{{ formattedFileSize }}</span>
     </template>
 
-    <template #bottom v-if="!isFileAlreadyExpired">
-      <Button theme="primary" @onClick="handleAction">Copy Link</Button>
+    <template #bottom>
+      <Button :theme="actionTheme" @onClick="handleAction">Copy Link</Button>
     </template>
   </BaseCard>
 </template>
@@ -20,19 +27,67 @@ import { computed, defineComponent } from 'vue'
 import { getReadableSize } from '../helpers'
 
 import BaseCard from '../BaseCard.vue'
+import { Button } from '@/components/Button'
 
 export default defineComponent({
   name: 'UploadCard',
-  components: { BaseCard },
+  components: { BaseCard, Button },
   props: {
     fileSize: { type: Number, required: true },
   },
-  setup(props) {
+  emits: ['onActionClick'],
+  setup(props, { emit }) {
     const formattedFileSize = computed(() => getReadableSize(props.fileSize))
+    const hasStatusMessage = computed(() => true)
+    const statusMessage = computed(() => 'File bigger than 100mb')
 
-    return { formattedFileSize }
+    const statusStyle = computed(() => ({}))
+    const actionTheme = computed(() => 'default')
+
+    const handleAction = () => emit('onActionClick')
+
+    return {
+      hasStatusMessage,
+      formattedFileSize,
+      statusStyle,
+      actionTheme,
+      handleAction,
+      statusMessage,
+    }
   },
 })
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.UploadCard {
+  &__head {
+    position: relative;
+    display: grid;
+    place-content: center;
+    padding: rem(25px 25px 0);
+    background: none;
+  }
+
+  &__circle {
+    $size: rem(144px);
+    position: relative;
+    width: $size;
+    height: $size;
+    border-radius: 50%;
+    border: 3px solid var(--color-shark);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: var(--concept-text-primary);
+
+    > h4 {
+      font-size: rem(16px);
+    }
+
+    &::before {
+      content: '';
+      position: absolute;
+    }
+  }
+}
+</style>

ade59493d7d9625c35d0ac4a4819a57d34717d56
emkis <nicolasemkis@gmail.com>
2021-03-14 03:44:55 -0300

    feat(client): add wrapped progress component

---
+++ web-client/src/components/Cards/UploadCard/UploadCardHead/UploadProgressCircle.vue
@@ -0,0 +1,29 @@
+<template>
+  <ProgressCircle
+    size="144"
+    :progress="uploadProgress"
+    :backgroundColor="backgroundColor"
+    :foregroundColor="foregroundColor"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { EThemeColors } from '@/services/theme'
+
+import ProgressCircle from '../ProgressCircle.vue'
+
+export default defineComponent({
+  name: 'UploadProgressCircle',
+  components: { ProgressCircle },
+  props: {
+    uploadProgress: { type: Number, default: 0 },
+    backgroundColor: { type: String, required: true },
+  },
+  setup() {
+    const foregroundColor = EThemeColors.shark
+
+    return { foregroundColor }
+  },
+})
+</script>

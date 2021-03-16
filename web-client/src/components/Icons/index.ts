c46ab9d86be2797085e7f76ecaabcb37765b9901
Nicolas Jardim <nicolasemkis@gmail.com>
2021-03-15 22:47:50 -0300

    refactor(client): remove icon barrer file

--- web-client/src/components/Icons/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as IconTimer } from './IconTimer.vue'
-export { default as IconFast } from './IconFast.vue'
-export { default as IconFlower } from './IconFlower.vue'
-export { default as IconHeart } from './IconHeart.vue'
-export { default as IconArrowLeft } from './IconArrowLeft.vue'
-export { default as IconFile } from './IconFile.vue'

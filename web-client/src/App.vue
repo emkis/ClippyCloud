61da83741b47170571b16f697cc14b059dd57a94
emkis <nicolasemkis@gmail.com>
2021-04-12 04:41:48 -0300

    feat(client): initialize app waking up api

--- web-client/src/App.vue
+++ web-client/src/App.vue
@@ -4,8 +4,12 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { WakeUpApi } from '@/services/api/wake-up-api'
 
 export default defineComponent({
   name: 'App',
+  setup() {
+    WakeUpApi.init()
+  },
 })
 </script>

a16b2f0f4e15f1be0c2ca040e35dba13aeb87721
emkis <nicolasemkis@gmail.com>
2021-04-14 01:33:04 -0300

    test(client): add function tests

---
+++ web-client/src/utilities/generators.test.ts
@@ -0,0 +1,15 @@
+import { generateUniqueId } from './generators'
+
+describe('generateUniqueId()', () => {
+  it('should create different IDs in a sequence', () => {
+    const key1 = generateUniqueId()
+    const key2 = generateUniqueId()
+
+    expect(key1).not.toBe(key2)
+  })
+
+  it('should return a string', () => {
+    const result = generateUniqueId()
+    expect(typeof result).toBe('string')
+  })
+})

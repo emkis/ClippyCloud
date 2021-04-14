ac055d1d529b805da8fa437251a8fc92595e4a01
emkis <nicolasemkis@gmail.com>
2021-04-14 02:46:39 -0300

    test(client): add file upload service tests

---
+++ web-client/src/services/api/file-upload/file-upload.test.ts
@@ -0,0 +1,38 @@
+import axios from 'axios'
+import { createFileUploadService } from './index'
+
+describe('FileUpload Service', () => {
+  beforeEach(jest.clearAllMocks)
+
+  describe('createFileUploadService()', () => {
+    it('should return expected functions', () => {
+      const service = createFileUploadService()
+
+      expect(service).toEqual({
+        upload: expect.any(Function),
+      })
+    })
+  })
+
+  describe('upload()', () => {
+    const axiosMock = axios.create()
+    const mockService = createFileUploadService(axiosMock)
+
+    axiosMock.post = jest.fn()
+
+    it('should be called with expected arguments', () => {
+      const mockUploadData = { userId: 'l48hfs', formData: new FormData() }
+
+      mockService.upload(mockUploadData)
+
+      expect(axiosMock.post).toBeCalledTimes(1)
+      expect(axiosMock.post).toHaveBeenCalledWith(
+        'user/l48hfs/files',
+        expect.any(FormData),
+        expect.objectContaining({ onUploadProgress: expect.any(Function) })
+      )
+    })
+
+    it.skip('should call onUploadProgress handler with progress', () => {})
+  })
+})

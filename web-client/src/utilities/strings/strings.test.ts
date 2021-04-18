cbaca4823cea8c54c9124da5dbfc062727335861
emkis <nicolasemkis@gmail.com>
2021-04-18 20:31:44 -0300

    test(client): add strings utilities tests

---
+++ web-client/src/utilities/strings/strings.test.ts
@@ -0,0 +1,30 @@
+import { truncate } from './index'
+
+describe('truncate()', () => {
+  it('should return the same text', () => {
+    const text = 'Gorila Roxo'
+
+    expect(truncate(text, 15)).toBe(text)
+  })
+
+  it('should return the text truncated and trimmed', () => {
+    const text = '777-666   '
+
+    expect(truncate(text, 7)).toBe('777-666...')
+  })
+
+  describe('should return truncated text with ellipsis', () => {
+    const testCases: [string, string, number][] = [
+      ['Lorem ipsum sit dolor amet niat', 'Lorem ipsum sit dolor a...', 23],
+      ['Hello There ', 'Hello T...', 7],
+      ['Noco Noco Senior', 'Noco...', 5],
+      ['a b c', 'a...', 2],
+    ]
+
+    testCases.forEach(([originalText, expectedText, limit]) => {
+      test(`text: "${originalText}" should be: "${expectedText}"`, () => {
+        expect(truncate(originalText, limit)).toBe(expectedText)
+      })
+    })
+  })
+})

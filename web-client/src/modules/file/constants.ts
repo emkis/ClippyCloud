a6f4d2eba5e84f70e013593d8b4c8e1bac59802a
emkis <nicolasemkis@gmail.com>
2021-04-19 02:17:42 -0300

    refactor(client): move file functions to module

--- web-client/src/modules/file/constants.ts
+++ web-client/src/modules/file/constants.ts
@@ -1,1 +1,4 @@
-export const FILE_MAX_SIZE = 100000000 // 100 MB
+import { getReadableSize } from '@/modules/file'
+
+export const FILE_MAX_SIZE = 104857600 // 100 MB
+export const FILE_MAX_SIZE_READABLE = getReadableSize(FILE_MAX_SIZE)
--- web-client/src/modules/file/functions.ts
+++ web-client/src/modules/file/functions.ts
@@ -1,3 +1,5 @@
+import filesize from 'filesize'
+
 import type { CustomFile } from './types'
 
 import { generateUniqueId } from '@/utilities/generators'
@@ -13,4 +15,9 @@ export function parseFile(file: File): CustomFile {
     hasInvalidSize: false,
     url: null,
   }
+}
+
+export function getReadableSize(size: number) {
+  const sizeFormatter = filesize.partial({ round: 0 })
+  return sizeFormatter(size)
 }

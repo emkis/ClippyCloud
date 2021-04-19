345f672138ea05767a6c538ba60cd391518492bf
emkis <nicolasemkis@gmail.com>
2021-04-19 03:05:46 -0300

    feat(client): add new action variant

--- web-client/src/components/Cards/UploadCard/UploadCardAction/actionFactory.ts
+++ web-client/src/components/Cards/UploadCard/UploadCardAction/actionFactory.ts
@@ -3,12 +3,13 @@ import { ECardVariants } from '../types'
 import ActionError from './variants/Error.vue'
 import ActionSuccess from './variants/Success.vue'
 import ActionInitial from './variants/Initial.vue'
+import ActionInvalidFileSize from './variants/InvalidFileSize.vue'
 
 const ActionVariants = {
   [ECardVariants.Success]: ActionSuccess,
   [ECardVariants.Error]: ActionError,
   [ECardVariants.Initial]: ActionInitial,
-  [ECardVariants.InvalidFileSize]: ActionError,
+  [ECardVariants.InvalidFileSize]: ActionInvalidFileSize,
 }
 
 export function makeActionVariant(variant: ECardVariants) {
---
+++ web-client/src/components/Cards/UploadCard/UploadCardAction/variants/InvalidFileSize.vue
@@ -0,0 +1,18 @@
+<template>
+  <Button @onClick="handleAction">Remove file</Button>
+</template>
+
+<script lang="ts">
+import { defineComponent, inject, Ref } from 'vue'
+import { Button } from '@/components/Button'
+
+export default defineComponent({
+  name: 'InvalidFileSize',
+  components: { Button },
+  setup() {
+    const handleAction = inject('actionHandler') as Ref<() => void>
+
+    return { handleAction }
+  },
+})
+</script>

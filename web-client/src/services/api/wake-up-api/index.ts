58c295ba2aa9101a23ceb2be9f37ce7ce3d8b7a8
emkis <nicolasemkis@gmail.com>
2021-04-24 01:21:57 -0300

    refactor(client): improve return type

--- web-client/src/services/api/wake-up-api/index.ts
+++ web-client/src/services/api/wake-up-api/index.ts
@@ -2,15 +2,15 @@ import { AxiosResponse } from 'axios'
 import { ApiService } from '../index'
 
 interface IWakeUpApi {
-  init(): Promise<AxiosResponse>
+  init(): Promise<AxiosResponse<void>>
 }
 
-export const createWakeUpApiService = (
-  httpClient = ApiService
-): IWakeUpApi => ({
-  init() {
-    return httpClient.get('wake-up')
-  },
-})
+export function createWakeUpApiService(httpClient = ApiService): IWakeUpApi {
+  return {
+    init() {
+      return httpClient.get('wake-up')
+    },
+  }
+}
 
 export const WakeUpApi = createWakeUpApiService()

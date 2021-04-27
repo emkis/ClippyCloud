6f8b01036e445770e92b55bba3c3b418fd446f58
emkis <nicolasemkis@gmail.com>
2021-04-26 23:42:14 -0300

    refactor(client): remove unnecessary props and change provided state

--- web-client/src/components/Cards/UploadCard/UploadCard.vue
+++ web-client/src/components/Cards/UploadCard/UploadCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <BaseCard class="UploadCard" :fileName="fileName">
+  <BaseCard :fileName="file.name">
     <template #top>
       <UploadCardHead />
     </template>
@@ -15,47 +15,40 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, provide } from 'vue'
+import { computed, defineComponent, PropType, provide } from 'vue'
 
 import { ECardVariants } from './types'
-import { defaultBaseCardProps } from '../defaultBaseCardProps'
 
 import BaseCard from '../BaseCard.vue'
 import { UploadCardHead } from './UploadCardHead'
 import { UploadCardAction } from './UploadCardAction'
 import { UploadCardDetail } from './UploadCardDetail'
 
+import type { CustomFile } from '@/modules/file'
+
 export default defineComponent({
   name: 'UploadCard',
   components: { BaseCard, UploadCardHead, UploadCardDetail, UploadCardAction },
   props: {
-    ...defaultBaseCardProps,
-    fileSize: { type: Number, required: true },
-    progress: { type: Number, required: true },
-    isFileInvalid: { type: Boolean, default: false },
-    isUploadFailed: { type: Boolean, default: false },
-    isCanceled: { type: Boolean, default: false },
+    file: { type: Object as PropType<CustomFile>, required: true },
   },
-  emits: ['onActionClick'],
-  setup(props, { emit }) {
-    const progress = computed(() => props.progress)
-    const fileSize = computed(() => props.fileSize)
-
-    const handleAction = () => emit('onActionClick')
-    const isUploadComplete = computed(() => props.progress >= 100)
-
-    const variant = computed(() => {
-      if (isUploadComplete.value) return ECardVariants.Success
-      if (props.isUploadFailed) return ECardVariants.Error
-      if (props.isFileInvalid) return ECardVariants.InvalidFileSize
-      if (props.isCanceled) return ECardVariants.Canceled
+  setup(props) {
+    const cardVariant = computed(() => {
+      if (props.file.isUploaded) return ECardVariants.Success
+      if (props.file.isUploadFailed) return ECardVariants.Error
+      if (props.file.isBiggerThanSizeLimit) return ECardVariants.InvalidFileSize
+      if (props.file.isUploadCanceled) return ECardVariants.Canceled
       else return ECardVariants.Initial
     })
 
-    provide('fileSize', fileSize)
+    const progress = computed(() => props.file.progress)
+    const fileId = computed(() => props.file.id)
+    const fileSize = computed(() => props.file.size)
+
+    provide('variant', cardVariant)
     provide('progress', progress)
-    provide('variant', variant)
-    provide('actionHandler', handleAction)
+    provide('fileId', fileId)
+    provide('fileSize', fileSize)
   },
 })
 </script>

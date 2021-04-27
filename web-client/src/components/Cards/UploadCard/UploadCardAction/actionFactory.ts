8cb8913f2f1b7ed90abdcb6ddeba8dd8b41b6dbb
emkis <nicolasemkis@gmail.com>
2021-04-27 00:38:11 -0300

    refactor(client): import variants into factory

--- web-client/src/components/Cards/UploadCard/UploadCardAction/actionFactory.ts
+++ web-client/src/components/Cards/UploadCard/UploadCardAction/actionFactory.ts
@@ -1,23 +1,17 @@
-import { h } from 'vue'
-
 import { ECardVariants } from '../types'
-import type { DefaultActionProps } from './actionDefaultProps'
 
-import DefaultAction from './variants/Default.vue'
-import SuccessAction from './variants/Success.vue'
-import ErrorAction from './variants/Error.vue'
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function withText(component: any, text: string) {
-  return h(component, { text } as DefaultActionProps)
-}
+import ActionSuccess from './variants/Success.vue'
+import ActionError from './variants/Error.vue'
+import ActionInitial from './variants/Initial.vue'
+import ActionInvalidFileSize from './variants/InvalidFileSize.vue'
+import ActionCanceled from './variants/Canceled.vue'
 
 const ActionVariants = {
-  [ECardVariants.Success]: withText(SuccessAction, 'Copy link'),
-  [ECardVariants.Error]: withText(ErrorAction, 'Try again'),
-  [ECardVariants.Initial]: withText(DefaultAction, 'Cancel'),
-  [ECardVariants.InvalidFileSize]: withText(DefaultAction, 'Remove file'),
-  [ECardVariants.Canceled]: withText(ErrorAction, 'Restart upload'),
+  [ECardVariants.Success]: ActionSuccess,
+  [ECardVariants.Error]: ActionError,
+  [ECardVariants.Initial]: ActionInitial,
+  [ECardVariants.InvalidFileSize]: ActionInvalidFileSize,
+  [ECardVariants.Canceled]: ActionCanceled,
 }
 
 export function makeActionVariant(variant: ECardVariants) {

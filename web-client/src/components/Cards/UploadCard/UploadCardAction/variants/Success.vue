82293050b8cfa6141975ecee2861ea7a461507f8
emkis <nicolasemkis@gmail.com>
2021-04-29 02:11:04 -0300

    feat(client): add copy link feedback

--- web-client/src/components/Cards/UploadCard/UploadCardAction/variants/Success.vue
+++ web-client/src/components/Cards/UploadCard/UploadCardAction/variants/Success.vue
@@ -1,9 +1,11 @@
 <template>
-  <Button theme="primary" @onClick="handleClick">Copy link</Button>
+  <Button theme="primary" @onClick="handleClick">
+    {{ isClicked ? 'Copied' : 'Copy link' }}
+  </Button>
 </template>
 
 <script lang="ts">
-import { defineComponent, inject, Ref } from 'vue'
+import { defineComponent, inject, ref, Ref } from 'vue'
 import { copyToClipboard } from '@/utilities/strings'
 
 import { Button } from '@/components/Button'
@@ -12,10 +14,16 @@ export default defineComponent({
   name: 'Success',
   components: { Button },
   setup() {
+    const isClicked = ref(false)
     const fileUrl = inject('fileUrl') as Ref<string>
-    const handleClick = () => copyToClipboard(fileUrl.value)
 
-    return { handleClick }
+    const handleClick = () => {
+      isClicked.value = true
+      setTimeout(() => (isClicked.value = false), 2000)
+      copyToClipboard(fileUrl.value)
+    }
+
+    return { isClicked, handleClick }
   },
 })
 </script>

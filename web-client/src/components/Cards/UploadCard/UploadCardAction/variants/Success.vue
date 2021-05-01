fb68f4a641a418dc8bb15991b7342f6c3e89cdfc
emkis <nicolasemkis@gmail.com>
2021-05-01 20:27:06 -0300

    refactor(client): remove local login to use component

--- web-client/src/components/Cards/UploadCard/UploadCardAction/variants/Success.vue
+++ web-client/src/components/Cards/UploadCard/UploadCardAction/variants/Success.vue
@@ -1,29 +1,19 @@
 <template>
-  <Button theme="primary" @onClick="handleClick">
-    {{ isClicked ? 'Copied' : 'Copy link' }}
-  </Button>
+  <ButtonCopy :value="fileUrl" />
 </template>
 
 <script lang="ts">
-import { defineComponent, inject, ref, Ref } from 'vue'
-import { copyToClipboard } from '@/utilities/strings'
+import { defineComponent, inject, Ref } from 'vue'
 
-import { Button } from '@/components/Button'
+import { ButtonCopy } from '@/components/ButtonCopy'
 
 export default defineComponent({
   name: 'Success',
-  components: { Button },
+  components: { ButtonCopy },
   setup() {
-    const isClicked = ref(false)
     const fileUrl = inject('fileUrl') as Ref<string>
 
-    const handleClick = () => {
-      isClicked.value = true
-      setTimeout(() => (isClicked.value = false), 2000)
-      copyToClipboard(fileUrl.value)
-    }
-
-    return { isClicked, handleClick }
+    return { fileUrl }
   },
 })
 </script>

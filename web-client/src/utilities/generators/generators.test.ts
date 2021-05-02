50aecca35071cf5e83172a7bd8a7a5c4dd9f86e2
emkis <nicolasemkis@gmail.com>
2021-05-02 19:11:18 -0300

    test(client): add new assetion for generators

--- web-client/src/utilities/generators/generators.test.ts
+++ web-client/src/utilities/generators/generators.test.ts
@@ -12,4 +12,10 @@ describe('generateUniqueId()', () => {
     const result = generateUniqueId()
     expect(typeof result).toBe('string')
   })
+
+  it('should have expected length', () => {
+    expect(generateUniqueId()).toHaveLength(8)
+    expect(generateUniqueId(3)).toHaveLength(3)
+    expect(generateUniqueId(10)).toHaveLength(10)
+  })
 })

0fa2f04fd16f3d225596b79edde43e0aee3d74ed
emkis <nicolasemkis@gmail.com>
2021-05-02 18:19:30 -0300

    refactor(api): change how file name is created

--- api/src/config/multer.ts
+++ api/src/config/multer.ts
@@ -1,7 +1,7 @@
 import multer from 'multer'
-import crypto from 'crypto'
 
 import { UPLOAD_FOLDER_PATH } from '../constants'
+import { generateUniqueId } from '../utilities/strings'
 
 export default {
   dest: UPLOAD_FOLDER_PATH,
@@ -11,11 +11,12 @@ export default {
       cb(null, UPLOAD_FOLDER_PATH)
     },
 
-    filename: (_, file: Express.Multer.File, cb) => {
-      crypto.randomBytes(16, (_, hash) => {
-        const customName = `${hash.toString('hex')}-${file.originalname}`
-        cb(null, customName)
-      })
+    filename: (_, file: Express.Multer.File, appendFileName) => {
+      const fileId = generateUniqueId()
+      const fileName = file.originalname
+      const customName = `${fileId}--${fileName}`
+
+      appendFileName(null, customName)
     },
   }),
 }

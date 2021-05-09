3b0b8e6ffaf287c2db7627e3d9bd4e2892a5beaf
emkis <nicolasemkis@gmail.com>
2021-05-09 03:18:08 -0300

    test(client): wraps assertion in describe

--- web-client/src/services/theme/theme.test.ts
+++ web-client/src/services/theme/theme.test.ts
@@ -1,9 +1,11 @@
 import { makeRgba } from './index'
 
 describe('Theme', () => {
-  it('should return correct rgba pattern', () => {
-    expect(makeRgba('var(--color)', 0)).toBe('rgba(var(--color), 0)')
-    expect(makeRgba('var(--color)', 0.25)).toBe('rgba(var(--color), 0.25)')
-    expect(makeRgba('var(--color)', 1)).toBe('rgba(var(--color), 1)')
+  describe('makeRgba()', () => {
+    it('should return correct rgba pattern', () => {
+      expect(makeRgba('var(--color)', 0)).toBe('rgba(var(--color), 0)')
+      expect(makeRgba('var(--color)', 0.25)).toBe('rgba(var(--color), 0.25)')
+      expect(makeRgba('var(--color)', 1)).toBe('rgba(var(--color), 1)')
+    })
   })
 })

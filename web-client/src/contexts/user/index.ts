d506eb84141d557d5926040c2fc396fcc44a0aba
emkis <nicolasemkis@gmail.com>
2021-05-12 01:14:07 -0300

    refactor(client): remove unnecessary state from context

--- web-client/src/contexts/user/index.ts
+++ web-client/src/contexts/user/index.ts
@@ -1,62 +1,29 @@
 import { ref, watch } from 'vue'
 
-import type { UserContextHook, UserState, UploadedFile } from './types'
+import type { UserContextHook, UserState } from './types'
 
-import {
-  getFromStorage,
-  removeFromStorage,
-  saveInStorage,
-} from '@/services/storage'
+import { getFromStorage, saveInStorage } from '@/services/storage'
 import { generateUniqueId } from '@/utilities/generators'
 
 const id = ref('')
-const uploadedFiles = ref<UploadedFile[]>([])
 
-watch([id, uploadedFiles], () => {
-  syncUserStateWithStorage()
-})
+watch([id], syncUserStateWithStorage)
 
 export function useUser(): UserContextHook {
-  return { id, uploadedFiles, addUploadedFile }
+  return { id }
 }
 
 export function initializeUser() {
   const user = getFromStorage<UserState>('user')
-  const storeUser = () => setUserId(generateUniqueId())
 
-  if (!user) storeUser()
-  else {
-    const isValidData = validateUser(user)
-    isValidData ? setUserState(user) : storeUser()
-  }
-}
-
-function validateUser(userData: UserState): boolean {
-  const { id, uploadedFiles } = userData
-
-  if (id && uploadedFiles) return true
-  else {
-    removeFromStorage('user')
-    return false
-  }
-}
-
-function addUploadedFile(file: UploadedFile) {
-  uploadedFiles.value = [file, ...uploadedFiles.value]
+  if (!user) setUserId(generateUniqueId())
+  else setUserId(user.id)
 }
 
 function setUserId(targetId: string) {
   id.value = targetId
 }
 
-function setUserState(userData: UserState) {
-  id.value = userData.id
-  uploadedFiles.value = userData.uploadedFiles
-}
-
 function syncUserStateWithStorage() {
-  saveInStorage<UserState>('user', {
-    id: id.value,
-    uploadedFiles: uploadedFiles.value,
-  })
+  saveInStorage<UserState>('user', { id: id.value })
 }

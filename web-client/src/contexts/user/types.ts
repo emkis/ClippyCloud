ce5f706d08463eb2b80a713d698d952fa7641a58
emkis <nicolasemkis@gmail.com>
2021-05-12 01:14:25 -0300

    refactor(client): remove unnecessary types from context

--- web-client/src/contexts/user/types.ts
+++ web-client/src/contexts/user/types.ts
@@ -2,20 +2,8 @@ import type { DeepReadonly, Ref } from 'vue'
 
 export interface UserContextHook {
   id: DeepReadonly<Ref<string>>
-  uploadedFiles: DeepReadonly<Ref<UploadedFile[]>>
-  addUploadedFile(file: UploadedFile): void
 }
 
 export interface UserState {
   id: string
-  uploadedFiles: UploadedFile[]
-}
-
-export interface UploadedFile {
-  id: string
-  name: string
-  url: string
-  size: number
-  extension?: string
-  createdAt: string
 }

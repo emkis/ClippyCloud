e8ea05a21a8b954198851ada24359cf7127cf65b
emkis <nicolasemkis@gmail.com>
2021-05-26 03:08:11 -0300

    test(client): remove unnecessary assertions

--- web-client/src/components/Navbar/Navbar.test.ts
+++ web-client/src/components/Navbar/Navbar.test.ts
@@ -6,23 +6,19 @@ interface INavbarProps {
   routeName: string
 }
 
-const mockUseRouter = {
-  push: jest.fn(),
-  back: jest.fn(),
-}
-
-jest.mock('vue-router', () => ({
-  useRouter: () => mockUseRouter,
-}))
-
 describe('<Navbar />', () => {
   beforeEach(jest.clearAllMocks)
 
   function renderNavbar(props: INavbarProps) {
-    return render(Navbar, { props })
+    return render(Navbar, {
+      props,
+      global: {
+        stubs: ['RouterLink'],
+      },
+    })
   }
 
-  it('should render links', async () => {
+  it('should render links correctly', async () => {
     const { findByTestId } = renderNavbar({
       linkName: 'Target Page Title',
       routeName: 'mockRouteName',
@@ -33,30 +29,5 @@ describe('<Navbar />', () => {
 
     expect(backLink).not.toBeNull()
     expect(externalLink).not.toBeNull()
-  })
-
-  test('when back link is pressed, should navigate to previous page', async () => {
-    const { findByTestId } = renderNavbar({
-      linkName: 'Target Page Title',
-      routeName: 'mockRouteName',
-    })
-
-    const backLink = await findByTestId('back-link')
-    await fireEvent.click(backLink)
-
-    expect(mockUseRouter.back).toHaveBeenCalledTimes(1)
-  })
-
-  test('when external link is pressed, should navigate to target route', async () => {
-    const { findByTestId } = renderNavbar({
-      linkName: 'Target Page Title',
-      routeName: 'mockRouteName',
-    })
-
-    const externalLink = await findByTestId('external-link')
-    await fireEvent.click(externalLink)
-
-    expect(mockUseRouter.push).toHaveBeenCalledTimes(1)
-    expect(mockUseRouter.push).toHaveBeenCalledWith({ name: 'mockRouteName' })
   })
 })
